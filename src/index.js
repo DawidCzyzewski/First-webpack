2eb93780a906a72a533083d7537f4f3d7828992f
Dawid Czyzewski <vinciebr@proton.me>
2023-03-06 22:05:40 +0100

    Comments

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// Firstly I created folders dist (and index.html in it - in this folder will be bundled code) and src (with index.js - here will be our source files). Next I've inictialized npm package.json (command npm init in terminal).
+// Secondly I installed webpack and webpack-client in devDependencies(command npm install webpack webpack-cli --save-dev).
+// Next step was to delate "main": index.js, in package.json and to add "private": true in this line. It helps us not to public our library by accident.
